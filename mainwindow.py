816c40a1418144e8da57db8b6bedba8f29b64581
elephark <indiagolfninerniner@gmail.com>
2023-11-03 23:28:54 -0700

    Major deducts and undo now work.

--- mainwindow.py
+++ mainwindow.py
@@ -31,6 +31,16 @@ class MainWindow(QMainWindow):
 
 		self.setWindowTitle("44clicker")
 
+		# Set up the exit strategy.
+		self.ui.actionQuit.triggered.connect(self.close)
+
+		# Set up the undo signal.
+		self.ui.actionUndoReset.triggered.connect(self.undoReset)
+
+		# Get the clicks ready.
+		self.curClicks = ClickScore()
+		self.lastClicks = ClickScore()
+		self.displayClicks()
 
 		# Initialize timers
 		self.fsTimer = QTimer(self)
@@ -43,11 +53,6 @@ class MainWindow(QMainWindow):
 		self.fsTimer.timeout.connect(self.timerFinished)
 		# self.timeRemaining = prefs.totalTimeSetting()
 		self.dispRefreshTimer.timeout.connect(self.timerRedraw)
-
-		self.curClicks = ClickScore()
-		self.lastClicks = ClickScore()
-
-		self.displayClicks()
 
 		self.timerRedraw()
 
@@ -64,7 +69,14 @@ class MainWindow(QMainWindow):
 	
 	@Slot()
 	def undoReset(self):
-		return
+		# Perform a single-level undo operation in case we reset accidentally.
+		self.curClicks.plusClicks = self.lastClicks.plusClicks
+		self.curClicks.minusClicks = self.lastClicks.minusClicks
+		self.curClicks.majDeductLv1Clicks = self.lastClicks.majDeductLv1Clicks
+		self.curClicks.majDeductLv2Clicks = self.lastClicks.majDeductLv2Clicks
+		self.curClicks.majDeductLv3Clicks = self.lastClicks.majDeductLv3Clicks
+
+		self.displayClicks()
 	
 	@Slot()
 	def showPrefDialog(self):
@@ -125,16 +137,58 @@ class MainWindow(QMainWindow):
 		self.displayClicks()
 	
 	def majorDeductClick(self, level:int):
-		return
+		if level == 1:
+			self.curClicks.majDeductLv1Clicks += 1 # todo: use prefs.mdWeight()
+		elif level == 2:
+			self.curClicks.majDeductLv2Clicks += 3 # see above
+		elif level == 3:
+			self.curClicks.majDeductLv3Clicks += 5 # see above
+
+		self.displayClicks()
 	
+
 	def displayClicks(self):
-		self.ui.plusClicksLcd.display(self.curClicks.plusClicks)
-		self.ui.minusClicksLcd.display(self.curClicks.minusClicks)
-		self.ui.totalClicksLcd.display(self.curClicks.plusClicks + 
-			self.curClicks.minusClicks)
+		clicks = self.curClicks
+		self.ui.plusClicksLcd.display(clicks.plusClicks)
+		self.ui.minusClicksLcd.display(clicks.minusClicks)
+		self.ui.totalClicksLcd.display(clicks.plusClicks + clicks.minusClicks)
+		self.ui.majDeductLv1Lcd.display(clicks.majDeductLv1Clicks)
+		self.ui.majDeductLv2Lcd.display(clicks.majDeductLv2Clicks)
+		self.ui.majDeductLv3Lcd.display(clicks.majDeductLv3Clicks)
+		self.ui.majDeductTotalLcd.display( \
+			clicks.majDeductLv1Clicks +
+			clicks.majDeductLv2Clicks +
+			clicks.majDeductLv3Clicks)
+		# Note that the majDeduct LCDs have two digits and will fail if you
+		# try to give them a three-digit number. If someone actually shows up
+		# and gets more than 99 major deducts in 3 minutes, though...
+		# I wouldn't even be mad. That would be legitimately impressive.
+
 	
 	def resetClicks(self):
-		return
+		# Make it so we can undo.
+		# Note that this doesn't check if anything was actually changed.
+		# If they start up, immediately reset, click some, and then undo,
+		# everything will "undo" back to zero, which is kind of weird.
+		# Further note that I don't much care what happens if someone
+		# really wants to do that.
+		self.ui.actionUndoReset.setEnabled(True)
+
+		# Back up first
+		self.lastClicks.plusClicks = self.curClicks.plusClicks
+		self.lastClicks.minusClicks = self.curClicks.minusClicks
+		self.lastClicks.majDeductLv1Clicks = self.curClicks.majDeductLv1Clicks
+		self.lastClicks.majDeductLv2Clicks = self.curClicks.majDeductLv2Clicks
+		self.lastClicks.majDeductLv3Clicks = self.curClicks.majDeductLv3Clicks
+
+		# Do the reset part
+		self.curClicks.plusClicks = 0
+		self.curClicks.minusClicks = 0
+		self.curClicks.majDeductLv1Clicks = 0
+		self.curClicks.majDeductLv2Clicks = 0
+		self.curClicks.majDeductLv3Clicks = 0
+
+		self.displayClicks()
 	
 	def timerStartPause(self, forceStop = False):
 		return
